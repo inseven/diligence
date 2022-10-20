4aee6b17a1ad6c7b845fd73b68ae87b4f7e2931e
Jason Barrie Morley <jbmorley@mac.com>
2022-10-20 17:01:10 -0400

    feat: Add a script to generate build numbers

---
+++ scripts/build-number.swift
@@ -0,0 +1,29 @@
+#!/usr/bin/env swift
+
+import Foundation
+
+func shell(_ command: String) -> String {
+    let task = Process()
+    let pipe = Pipe()
+    task.standardOutput = pipe
+    task.standardError = pipe
+    task.arguments = ["-c", command]
+    task.launchPath = "/bin/zsh"
+    task.standardInput = nil
+    task.launch()
+    let data = pipe.fileHandleForReading.readDataToEndOfFile()
+    let output = String(data: data, encoding: .utf8)!
+    return output
+}
+
+let now = Date()
+let dateFormatter = DateFormatter()
+dateFormatter.dateFormat = "yyMMddHHmm"
+dateFormatter.timeZone = TimeZone(abbreviation: "UTC")
+let dateValue = dateFormatter.string(from: now)
+
+let sha = shell("git rev-parse --short=6 HEAD").trimmingCharacters(in: .whitespacesAndNewlines)
+let shaValue = Int(sha, radix: 16)!
+
+let build = String(format: "%@%08d", dateValue, shaValue)
+print(build)
