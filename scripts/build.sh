37868829f56e5bef5bd66a27e11d64c1fc5a429a
Jason Morley <jbmorley@mac.com>
2022-10-31 07:59:34 -0600

    ci: Automated build and release (#6)

---
+++ scripts/build.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+
+set -e
+set -o pipefail
+set -x
+
+SCRIPT_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+ROOT_DIRECTORY="${SCRIPT_DIRECTORY}/.."
+
+cd "$ROOT_DIRECTORY"
+
+xcodebuild -scheme Diligence -showdestinations
+xcodebuild -scheme Diligence -destination "platform=macOS" clean build
+xcodebuild -scheme Diligence -destination "platform=iOS Simulator,name=iPhone 14 Pro" clean build
