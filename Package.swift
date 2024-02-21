f3203e25c3ab11068eae7efcb64d0d261e70c915
Jason Morley <hello@jbmorley.co.uk>
2024-02-20 17:29:51 -1000

    Simplify the package definition a little

--- Package.swift
+++ Package.swift
@@ -1,5 +1,4 @@
 // swift-tools-version:5.5
-// The swift-tools-version declares the minimum version of Swift required to build this package.
 
 import PackageDescription
 
@@ -10,7 +9,6 @@ let package = Package(
         .macOS(.v12)
     ],
     products: [
-        // Products define the executables and libraries a package produces, and make them visible to other packages.
         .library(
             name: "Diligence",
             targets: ["Diligence"]),
@@ -19,8 +17,6 @@ let package = Package(
         .package(url: "https://github.com/inseven/licensable", from: "0.0.3"),
     ],
     targets: [
-        // Targets are the basic building blocks of a package. A target can define a module or a test suite.
-        // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "Diligence",
             dependencies: [
